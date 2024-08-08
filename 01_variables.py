fc53648833add7841925193352159b7819402aa7
Yojana Sajja <yojana@Yojanas-Laptop.attlocal.net>
2024-08-08 12:35:22 -0500

    variables

--- 01_variables.py
+++ 01_variables.py
@@ -1,2 +1,20 @@
-a = 42
+a = 42   # This is an integer
 print(a)
+
+#Float
+b = 23.54342
+print(b)
+
+# boolean
+c = True
+print(c)
+
+# Strings
+d = 'This is string1'
+e = "This is string2"
+f = """
+This is string3 or multi string
+"""
+#Override the value of a variable
+d = 'Today's weather is good'
+print(d)
